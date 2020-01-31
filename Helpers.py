a33cefe47102dcb910623901b010a990090e5aeb
mlo31415 <mlo@baskerville.org>
2020-01-30 21:07:10 -0500

    Return empty strings as None

--- Helpers.py
+++ Helpers.py
@@ -6,13 +6,17 @@ def ToNumeric(val):
     if val == None:
         return None
 
-    if isinstance(val, int) or isinstance(val, float):
+    if isinstance(val, str) and len(val.strip()) == 0:  # Empty strings become None
+        return None
+
+    if isinstance(val, int) or isinstance(val, float):  # Numbers are numbers and just get returned
         return val
 
+    # Last chance is to try to convert the vale into an int or float.
     try:
         return int(val)
     except:
         try:
             return float(val)
         except:
-            return val
+            return val  #Foiled on all fronts; Just return the input value -- whatever it is.
